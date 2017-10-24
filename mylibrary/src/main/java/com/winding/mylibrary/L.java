5e9b4606cc5c1fb7badf99287319444b24b139be
KiwiLss <m15205622327@163.com>
2017-10-24 13:51:44 +0800

    10-24

---
+++ mylibrary/src/main/java/com/winding/mylibrary/L.java
@@ -0,0 +1,63 @@
+package com.winding.mylibrary;
+
+import com.alibaba.fastjson.JSON;
+import com.orhanobut.logger.Logger;
+
+/**
+ * 日志相关工具类
+ */
+public class L {
+    public static final String TAG = "MMM";
+    public static boolean isLog=true;
+
+    public static void setIsLog(boolean isShow){
+        isLog=isShow;
+    }
+
+    public static void d(Object o){
+        if (isLog) {
+            Logger.t(TAG).d(o.toString());
+        }
+    }
+    public static void i(Object o){
+        if (isLog) {
+            Logger.t(TAG).i(o.toString());
+        }
+    }
+
+    public static void e(Object o){
+        if (isLog) {
+            Logger.t(TAG).e(o.toString());
+        }
+    }
+    public static void w(Object o){
+        if (isLog) {
+            Logger.t(TAG).w(o.toString());
+        }
+    }
+    public static void json(String o){
+        if (isLog) {
+            Logger.t(TAG).json(o);
+        }
+    }
+    public static void xml(String o){
+        if (isLog) {
+            Logger.t(TAG).xml(o);
+        }
+    }
+
+    /**object --> json type
+     * @param o
+     */
+    public static void ii(Object o){
+        if (isLog) {
+            Logger.t(TAG).i(JSON.toJSONString(o));
+        }
+    }
+    public static void ee(Object o){
+        if (isLog) {
+            Logger.t(TAG).e(JSON.toJSONString(o));
+        }
+    }
+
+}
