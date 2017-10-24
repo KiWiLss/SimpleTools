843e32074a3de077e2374b1adacdf12d753ae2e3
KiwiLss <m15205622327@163.com>
2017-10-24 15:51:53 +0800

    10-24

--- mylibrary/src/main/java/com/winding/mylibrary/MyApp.java
+++ mylibrary/src/main/java/com/winding/mylibrary/MyApp.java
@@ -1,6 +1,7 @@
 package com.winding.mylibrary;
 
 import android.app.Application;
+import android.content.Context;
 
 import com.orhanobut.logger.AndroidLogAdapter;
 import com.orhanobut.logger.Logger;
@@ -9,11 +10,19 @@ import com.orhanobut.logger.Logger;
  * Created by 刘少帅 on 2017/10/24
  */
 
-public class App extends Application {
+public class MyApp extends Application {
+
+    private static Context context;
+
     @Override
     public void onCreate() {
         super.onCreate();
+        context = getApplicationContext();
         //初始化可打印日志
         Logger.addLogAdapter(new AndroidLogAdapter());
+    }
+
+    public static Context getContext(){
+        return context;
     }
 }
